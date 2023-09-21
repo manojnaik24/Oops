b2fc434f26bb52970fe009907d9fa1fa3444240c
Manoj naik <chittinaik@gamil.com>
2023-09-21 19:34:32 +0530

    [Manoj] add:Object and Class

---
+++ OopsPracticeProblem/OopsPracticeProblem/ObjectCalss.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace OopsPracticeProblem
+{
+    internal class ObjectCalss
+    {
+        public void Print()
+        {
+            Console.WriteLine("Enter the Name:");
+            string name=Console.ReadLine();
+            Console.WriteLine( name);
+        }
+    }
+}
---
+++ OopsPracticeProblem/OopsPracticeProblem/Program.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace OopsPracticeProblem
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Oops Concept Practice Problem");
+            Console.WriteLine("1) Create a program with object and class concept with example."+
+                              "\n2) Create a program with Inheritance concept with example."+
+                              "\n0) Quit");
+            int n=int.Parse(Console.ReadLine());
+            if (n == 0)
+            {
+                Console.WriteLine("Enter the valid Input");
+                return;
+            }
+            else
+            {
+                switch (n)
+                {
+                    case 1:
+
+                        ObjectCalss o= new ObjectCalss();
+                        o.Print();
+                        break;
+
+
+
+                }
+            }
+            Console.ReadLine();
+        }
+    }
+}
